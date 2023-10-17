0de8186418908924e47824240784d2e939d991a4
Michael Prenez-Isbell <prenezisbell@gmail.com>
2023-10-17 07:44:23 -0400

    Initial Commit

---
+++ OptionalOptional/OptionalOptionalApp.swift
@@ -0,0 +1,17 @@
+//
+//  OptionalOptionalApp.swift
+//  OptionalOptional
+//
+//  Created by michael isbell on 10/17/23.
+//
+
+import SwiftUI
+
+@main
+struct OptionalOptionalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
